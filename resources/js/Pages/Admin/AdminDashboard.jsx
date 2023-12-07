e3ac23cf2761a9738d4a9e9d1ba07aa1b57632b0
Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
2023-12-08 01:17:04 +0700

    Update AdminDashboard.jsx
    Added Checkout Menu Button
    Added Inventory Menu Button
    Actual menus are not yet implemented

--- resources/js/Pages/Admin/AdminDashboard.jsx
+++ resources/js/Pages/Admin/AdminDashboard.jsx
@@ -9,13 +9,105 @@ export default function AdminDashboard({ auth }) {
         >
             <Head title="Admin Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in! Admin</div>
+            <div className="max-w-7xl mx-auto p-6 lg:p-8">
+
+                    <div className="mt-16">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                            <a
+                                href={route('admin.dashboard')}
+                                className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
+                            >
+                                <div>
+                                    <div className="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth="1.5"
+                                            className="w-7 h-7 stroke-red-500"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                                            />
+                                        </svg>
+                                    </div>
+
+                                    <h2 className="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
+                                        Checkout
+                                    </h2>
+
+                                    <p className="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                        Checkout barang. NOT IMPLEMENTED.
+                                    </p>
+                                </div>
+
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    className="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                                    />
+                                </svg>
+                            </a>
+
+                            <a
+                                href={route('admin.dashboard')}
+                                className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
+                            >
+                                <div>
+                                    <div className="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth="1.5"
+                                            className="w-7 h-7 stroke-red-500"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+                                            />
+                                        </svg>
+                                    </div>
+
+                                    <h2 className="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
+                                        Inventory
+                                    </h2>
+
+                                    <p className="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                        Manage Stok Sparepart
+                                    </p>
+                                    <p className="text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                        Manage Stok Mesin
+                                    </p>
+                                </div>
+
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    className="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                                    />
+                                </svg>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
+
         </AuthenticatedLayout>
     );
 }
